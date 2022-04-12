79cda4b44d6b243b0c2d0c95add13a163965d08c
Raj gupta <65468161+rajgupta93@users.noreply.github.com>
2022-04-12 23:13:30 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -8,43 +8,61 @@ class Solution
 	public:
 	//Function to return list containing vertices in Topological order. 
 	
-	void dfs(int V,vector<int> adj[],stack<int> &s,vector<int> &ans,int vis[])
+	vector<int> kahntopo(int V,vector<int> adj[],vector<int> indegree)
 	{
-	    
-	  
-	    for(int i=0;i<adj[V].size();i++)
+	    queue<int> q;
+	    vector<int> ans;
+	    for(int i=0;i<V;i++)
 	    {
-	       if(!vis[adj[V][i]])
-	       {
-	              
-	               vis[adj[V][i]]=1;
-	   dfs(adj[V][i],adj,s,ans,vis);
-	       }
+	      if(indegree[i]==0)
+	        q.push(i);
+	       // cout<<i<<" ";
+	       // break;
 	    }
-	     s.push(V);
+	    int count=0;
+	    
+	    while(!q.empty())
+	    {
+	        int i=q.front();
+	        q.pop();
+	        count++;
+	        ans.push_back(i);
+	        for(int j=0;j<adj[i].size();j++)
+	        {
+	            indegree[adj[i][j]]--;
+	            if(indegree[adj[i][j]]==0)
+	        q.push(adj[i][j]);
+	            
+	        }
+	       
+	    
+	    }
+	    
+	   // if(count!=v)
+	        return  ans;
+	    
+	    
 	}
+	
 	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    int vis[V]={0};
-	    stack<int> s;
-	    vector<int> ans;
+	    vector<int> indegree(V,0);
+	    
 	    for(int i=0;i<V;i++)
 	    {
-	        if(!vis[i])
+	        for(int j=0;j<adj[i].size();j++)
 	        {
-	        vis[i]=1;
-	            dfs(i,adj,s,ans,vis);
+	            indegree[adj[i][j]]++;
 	        }
 	    }
 	    
+	   // cout<<indegree[0];
+	   vector<int> ans= kahntopo(V,adj,indegree);
 	    
-	    while(!s.empty())
-	    {
-	        ans.push_back(s.top());
-	        s.pop();
-	    }
+	   // for(int i=0;i<ans.size();i++)
+	   //     cout<<ans[i]<<" ";
 	    return ans;
 	}
 };
