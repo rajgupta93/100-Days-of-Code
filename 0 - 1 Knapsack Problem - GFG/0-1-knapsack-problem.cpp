6e23ca3d066a77ad301d224d2ccf96175587b24d
Raj gupta <65468161+rajgupta93@users.noreply.github.com>
2022-04-29 22:21:04 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -14,26 +14,34 @@ class Solution
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       if(k==0)
-    {memset(dp,-1,sizeof(dp));
-        k++;
-    }   
-       if(n==0||W==0)
-        return 0;
+    //   if(k==0)
+    // {memset(dp,-1,sizeof(dp));
+    //     k++;
+    // }   
+    //   if(n==0||W==0)
+    //     return 0;
         
-        if(dp[W][n]!=-1)
-        {
-            return dp[W][n];
-        }
+        for(int i=0;i<W+1;i++)
+            for(int j=0;j<n+1;j++)
+                if(i==0||j==0)
+                    dp[i][j]=0;
         
-        if(W<wt[n-1])
+        
+     for(int i=1;i<n+1;i++)
+     {
+         for(int j=1;j<W+1;j++)
+         {
+            if(j<wt[i-1])
         {
-           return dp[W][n]= knapSack(W,wt,val,n-1);
-        }
-        else
+           dp[i][j]=dp[i-1][j];
+        }else
         {
-            return dp[W][n]=max(knapSack(W,wt,val,n-1),val[n-1]+knapSack(W-wt[n-1],wt,val,n-1));
+         dp[i][j]= max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+    
         }
+         }
+     }
+        return dp[n][W];
     }
 };
 
