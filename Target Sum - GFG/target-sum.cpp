6141adf455f7ba8a3d7c243822c5e7f8fed3ac65
Raj gupta <65468161+rajgupta93@users.noreply.github.com>
2022-05-03 18:20:55 +0530

    Added solution - LeetHub

--- Target Sum - GFG/target-sum.cpp
+++ Target Sum - GFG/target-sum.cpp
@@ -10,28 +10,27 @@ using namespace std;
 
 class Solution {
   public:
-
- 
-
-int dp[25][20000];
- int find(vector<int>&A,int target,int sum,int n)
- {
-     if(n==0&&sum==target)
-     return 1;
-     if(n<=0)
-     return 0;
-     if(dp[n][sum]!=-1)
-     return dp[n][sum];
-     return dp[n][sum]=find(A,target,sum-A[n-1],n-1)+find(A,target,sum+A[n-1],n-1);
- }
-
-
   
-   int findTargetSumWays(vector<int>&A ,int target) {
+  int  dp[21][20000];
+  int findtarg(vector<int>&A ,int target,int n)
+  {
+      if(n==0 && target==0)
+        return 1;
+        
+        if(n==0)
+            return 0;
+            
+            if(dp[n][target]!=-1)
+                return dp[n][target];
+            
+        return dp[n][target]= (findtarg(A,target+A[n-1],n-1) + findtarg(A,target-A[n-1],n-1));
+  }
+  
+    int findTargetSumWays(vector<int>&A ,int target) {
         //Your code here
         // int dp[A.size()+1][target+1];
-          memset(dp,-1,sizeof(dp));
-       return find(A,target,0,A.size());
+        memset(dp,-1,sizeof(dp));
+    return    findtarg(A,target,A.size());
         
     }
 };
