93ad67bc7bb16b154529f24e7e97eba5df92a0e8
Raj gupta <65468161+rajgupta93@users.noreply.github.com>
2022-05-06 14:03:09 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.24%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -5,8 +5,8 @@ public:
     int climbStairs(int n) {
         if(n==0)
             return 1;
-        if(n<0)
-            return 0;
+        if(n==1)
+            return 1;
       if(arr[n]!=0)
           return arr[n];
             return arr[n]=climbStairs(n-1)+climbStairs(n-2);
