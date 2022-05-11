2505bdccc309a421a321627e08c55679ad7792ed
Raj gupta <65468161+rajgupta93@users.noreply.github.com>
2022-05-11 23:54:29 +0530

    Time: 162 ms (5.47%), Space: 5.8 MB (98.39%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    // int dp[51][6];
+    
+//     int nextele(char c)
+//     {
+//         if(c=='a')
+//             return "5";
+//         if(c=='e')
+//             return "4";
+//         if(c=='i')
+//             return "3";
+//         if(c=='o')
+//             return "2";
+//         if(c=='u')
+//             return "1";
+//         else
+//             return "5";
+//     }
+ 
+    
+    int countvowel(int n,int numchar)
+    {
+        if(n==0)
+        {
+           
+            return 1;
+        }
+        int count=0;
+      for(int i=1;i<=numchar;i++){
+          count+=countvowel(n-1,i);
+      }
+       
+        return count;
+    }
+    
+    int countVowelStrings(int n) {
+        
+    // memset(dp,-1,sizeof(dp));
+        
+   return    countvowel(n,5);
+        // return count;
+    }
+};
