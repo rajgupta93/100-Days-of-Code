ac97d422e2ff1a07b64aae5dabac570e3ac86c38
Raj gupta <65468161+rajgupta93@users.noreply.github.com>
2022-05-17 00:44:39 +0530

    Time: 79 ms (68.60%), Space: 19 MB (97.53%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,53 +1,50 @@
 class Solution {
 public:
     
-    bool isValid(vector<vector<int>>& grid, int i, int j, int n, vector<vector<bool>>& visited){
-        
-        return (i>=0 and i<n and j>=0 and j<n and grid[i][j]==0 and !visited[i][j]);
-        
+    bool isvalid(int row,int col,int rs,int cs)
+    {
+        if(row<0 || col<0 || row>= rs || col>=cs)
+            return false;
+        return true;
     }
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         
-        int n = grid.size();
-        vector<vector<bool>> visited(n, vector<bool> (n, false));
-        queue<pair<int, int>> q;
-        int ans = 0;
-        int nodesPushed;
+        int dir[][2]={{1,1},{-1,-1},{0,1},{0,-1},{1,-1},{1,0},{-1,0},{-1,1}};
         
-        if(grid[0][0] == 0){
-            q.push({0, 0});
-            visited[0][0] = true;
-        }
-                
-        while(!q.empty()){
-            
-            nodesPushed = q.size();
-            ans++;
-            
-            for(int cnt = 0; cnt < nodesPushed; cnt++){
-                
-                pair<int, int> frontNode = q.front();
-                q.pop();
-            
-                int i = frontNode.first, j = frontNode.second;
-
-                if(i==n-1 and j==n-1) return ans;
-
-                for(int k = i - 1; k <= i + 1 ; k++){
-                    for(int l = j - 1; l <= j + 1; l++){
-                        if(isValid(grid, k, l, n, visited)){
-                            q.push({k, l});
-                            visited[k][l] = true;
-                        }
-                    }
-                }                
-                
-            }            
-            
-        }
         
+        queue<pair<int,int>> q;
+        
+        if(!grid[0][0]) q.push({0,0});
+        int ans=0;
+        
+        while(!q.empty())
+        {
+             int size=q.size();
+                ans++;
+            while(size--){
+                
+            
+              
+            pair<int,int> temppair=q.front();
+            q.pop();
+        
+          
+           
+            
+            if(temppair.first== grid.size()-1 && temppair.second== grid[0].size()-1)
+                    return ans;
+            for(int i=0;i<8;i++){
+                
+                int row=temppair.first+dir[i][0];
+                int col=temppair.second+dir[i][1];
+                
+                if( isvalid(row,col,grid.size(),grid[0].size()) && !grid[row][col])                     {    grid[row][col]=1;
+                    q.push({row,col});}
+            }
+        }
+        }
+
         return -1;
-        
     }
 };
