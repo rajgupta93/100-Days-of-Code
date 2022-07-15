bcb2560c900799473921d36c5f38ba4a66919fcc
Raj gupta <65468161+rajgupta93@users.noreply.github.com>
2022-07-15 23:18:08 +0530

    Time: 23 ms (79.42%), Space: 23.2 MB (67.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -4,18 +4,14 @@ public:
     int dfs(vector<vector<int>> &grid,int r,int c,int er,int ec,int count)
     {
         
-        if(r>=er || c>=ec || r<0 || c<0|| grid[r][c]==0 )
-          {
-            return 0;}
+        if(r>=er || c>=ec || r<0 || c<0|| grid[r][c]==0 ) return 0;
         
         grid[r][c]=0;
-        // count++;
         
        int x= dfs(grid,r,c-1,er,ec,count);
         int y=dfs(grid,r+1,c,er,ec,count);
        int z= dfs(grid,r,c+1,er,ec,count);
        int t= dfs(grid,r-1,c,er,ec,count);
-        // dfs(grid,r,c,er,ec);
         
         return (1+x+y+z+t);
         
