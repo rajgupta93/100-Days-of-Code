0d31afa288148358ad36c7302759fd9f0a9eb412
Raj gupta <65468161+rajgupta93@users.noreply.github.com>
2022-08-01 11:30:39 +0530

    Time: 4 ms (28.94%), Space: 6 MB (63.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     
-     int dp[101][101];
+    int dp[101][101];
+    int noOfPath(int r, int c, int m, int n) {
     
-     int uniquePath(int m, int n) {
-         if(m < 1  || n < 1 ) return 0;
-        if(m == 1 && n == 1) return 1;
-         
-         if(dp[m][n]!=-1) return dp[m][n]; 
-         return dp[m][n]=uniquePath(m-1,n) + uniquePath(m,n-1);
+        if(r == m-1 && c == n-1) return 1;
+        if(r > m-1 || c > n-1) return 0;
+        if(dp[r][c] != -1) return dp[r][c];
+        
+        return dp[r][c] = (noOfPath(r+1, c, m, n) + noOfPath(r, c+1, m, n));
     }
-  
+    
+    
     
     int uniquePaths(int m, int n) {
-   
         memset(dp,-1,sizeof(dp));
-        return uniquePath(m,n);
-        
+        return noOfPath(0,0,m,n);
     }
 };
